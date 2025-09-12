32df3a8f15841c320e78488775f7f79fd9987cce
jesus-z <jedu_1010@hotmail.com>
2025-09-11 20:25:14 -0400

    Migracion en clase para prueba de pagina funcionando

---
+++ mvcProyect/Migrations/20250911225242_datita.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mvcProyect.Migrations
+{
+    /// <inheritdoc />
+    public partial class datita : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
